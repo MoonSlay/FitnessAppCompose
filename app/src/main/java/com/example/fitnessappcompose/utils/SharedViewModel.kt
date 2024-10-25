61f312c27364a8f1373ad7932c59675db40617f7
Angelo <delrosarioangelo18@gmail.com>
2024-10-25 14:22:43 +0800

    implemented shared preference to total calories burned

--- app/src/main/java/com/example/fitnessappcompose/utils/SharedViewModel.kt
+++ app/src/main/java/com/example/fitnessappcompose/utils/SharedViewModel.kt
@@ -1,24 +1,29 @@
 // SharedViewModel.kt
 package com.example.fitnessappcompose.utils
 
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.example.fitnessappcompose.ui.screens.Training
 
-class SharedViewModel : ViewModel() {
+class SharedViewModel(application: Application) : AndroidViewModel(application) {
+    private val sharedPreferences = application.getSharedPreferences("fitness_prefs", Context.MODE_PRIVATE)
     private val _selectedTraining = MutableLiveData<Training>()
     val selectedTraining: LiveData<Training> get() = _selectedTraining
 
     private val _caloriesBurned = MutableLiveData<Int>()
     val caloriesBurned: LiveData<Int> get() = _caloriesBurned
 
-    private val _totalCaloriesBurned = MutableLiveData<Int>(0)
+    private val _totalCaloriesBurned = MutableLiveData<Int>(sharedPreferences.getInt("total_calories_burned", 0))
     val totalCaloriesBurned: LiveData<Int> get() = _totalCaloriesBurned
 
     fun setCaloriesBurned(calories: Int) {
         _caloriesBurned.value = calories
-        _totalCaloriesBurned.value = (_totalCaloriesBurned.value ?: 0) + calories
+        val newTotal = (_totalCaloriesBurned.value ?: 0) + calories
+        _totalCaloriesBurned.value = newTotal
+        sharedPreferences.edit().putInt("total_calories_burned", newTotal).apply()
     }
 
     fun selectTraining(training: Training) {
