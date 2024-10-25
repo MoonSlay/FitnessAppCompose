e2465c23bfc07d239a3538ef0f2cccc07c124e69
Angelo <delrosarioangelo18@gmail.com>
2024-10-25 15:05:09 +0800

    added daily, weekly, monthly step count to report screen

--- app/src/main/java/com/example/fitnessappcompose/MainActivity.kt
+++ app/src/main/java/com/example/fitnessappcompose/MainActivity.kt
@@ -18,11 +18,13 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import com.example.compose.AppTheme
 import com.example.fitnessappcompose.navigation.BottomNavigationBar
 import com.example.fitnessappcompose.navigation.NavigationGraph
+import com.example.fitnessappcompose.utils.SharedViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
@@ -38,6 +40,8 @@ class MainActivity : ComponentActivity(), SensorEventListener {
     private val _stepCount = MutableStateFlow(0)
     val stepCount: StateFlow<Int> get() = _stepCount
 
+    private lateinit var sharedViewModel: SharedViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -47,6 +51,8 @@ class MainActivity : ComponentActivity(), SensorEventListener {
         // Retrieve step count from SharedPreferences
         val sharedPreferences = getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
         _stepCount.value = sharedPreferences.getInt("step_count", 0)
+
+        sharedViewModel = ViewModelProvider(this).get(SharedViewModel::class.java)
 
         setContent {
             AppTheme {
@@ -103,15 +109,13 @@ class MainActivity : ComponentActivity(), SensorEventListener {
     override fun onSensorChanged(sensorEvent: SensorEvent?) {
         sensorEvent?.let { event ->
             if (event.sensor.type == Sensor.TYPE_STEP_DETECTOR) {
-                _stepCount.update { it + 1 }
-                // Save step count to SharedPreferences
-                val sharedPreferences = getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
-                with(sharedPreferences.edit()) {
-                    putInt("step_count", _stepCount.value)
-                    apply()
-                }
+                updateStepCount()
             }
         }
+    }
+
+    private fun updateStepCount() {
+        sharedViewModel.setStepCount(1)
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
